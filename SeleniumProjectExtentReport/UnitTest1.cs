934ddbca14c36fbb266eb77c74c5227a8fb0f0f8
Usama Mushtaq <usamamushtaq03@gmail.com>
2024-11-25 16:03:31 +0500

    comment.

--- SeleniumProjectExtentReport/UnitTest1.cs
+++ SeleniumProjectExtentReport/UnitTest1.cs
@@ -1,5 +1,14 @@
 using AventStack.ExtentReports.Reporter;
 using AventStack.ExtentReports;
+using static System.Net.WebRequestMethods;
+using System.Reflection;
+using System.IO;
+using System.Diagnostics;
+using System.IO;
+using AventStack.ExtentReports.Model;
+using OpenQA.Selenium.BiDi.Modules.Script;
+using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
+
 
 namespace SeleniumProjectExtentReport
 {
@@ -9,9 +18,17 @@ namespace SeleniumProjectExtentReport
         [TestMethod]
         public  void UnitTest()
         {
+            //get current directory
+
+            var path = Environment.CurrentDirectory;
+            //"C:\\Users\\mushtusa\\Source\\Repos\\SeleniumProjectExtentReport\\SeleniumProjectExtentReport\\bin\\Debug\\net8.0"
+
+            var parent = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(path).ToString().ToString()).ToString()).ToString()).ToString();
+
+
             ExtentReports extent = new ExtentReports();
 
-            var htmlReporter = new ExtentSparkReporter("00.html");
+            var htmlReporter = new ExtentSparkReporter($"{parent}\\ExtentReport\\00.html");
 
             extent.AttachReporter(htmlReporter);
 
